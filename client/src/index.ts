f799a1bf75eca628f2c19be10e329acf20c116b8
erics118 <52634785+erics118@users.noreply.github.com>
2024-05-16 01:29:29 -0400

    remove fps selector

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,8 +3,6 @@ import Phaser from "phaser";
 import { Scene } from "./scenes/Scene";
 import { SceneSelector } from "./scenes/SceneSelector";
 
-import { BACKEND_HTTP_URL } from "./backend";
-
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   fps: {
@@ -25,30 +23,3 @@ const config: Phaser.Types.Core.GameConfig = {
 };
 
 const game = new Phaser.Game(config);
-
-/**
- * Create FPS selector
- */
-
-// current fps label
-const fpsInput = document.querySelector<HTMLInputElement>("input#fps");
-const fpsValueLabel = document.querySelector<HTMLSpanElement>("#fps-value");
-fpsValueLabel.innerText = fpsInput.value;
-
-fpsInput.oninput = (event: InputEvent) => {
-  const value = (event.target as HTMLInputElement).value;
-  fpsValueLabel.innerText = value;
-
-  // destroy previous loop
-  game.loop.destroy();
-
-  // create new loop
-  game.loop = new Phaser.Core.TimeStep(game, {
-    target: Number.parseInt(value),
-    forceSetTimeOut: true,
-    smoothStep: false,
-  });
-
-  // start new loop
-  game.loop.start(game.step.bind(game));
-};
