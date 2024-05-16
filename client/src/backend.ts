ccc1f36a15a49ad9cec33fddc1136e2dabaf5fb3
erics118 <52634785+erics118@users.noreply.github.com>
2024-05-16 02:24:43 -0400

    fix backend issues a bit
    BACKEND_URL needs to be set to ws://SERVER_IP:2567 and everything works properly

--- client/src/backend.ts
+++ client/src/backend.ts
@@ -1,7 +1,8 @@
-export const BACKEND_URL =
-  window.location.href.indexOf("localhost") === -1
-    ? // biome-ignore lint: dumb
-      process.env.BACKEND_URL!
-    : "ws://localhost:2567";
+//  biome-ignore lint: dumb
+export const BACKEND_URL = process.env.BACKEND_URL ?? "ws://192.168.68.111:2567"
+// window.location.href.indexOf("localhost") === -1
+//   ? // biome-ignore lint: dumb
+//     process.env.BACKEND_URL!
+//   : "ws://localhost:2567";
 
 export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", "http");
