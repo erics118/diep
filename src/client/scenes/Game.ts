31ed5254d8e9e087d2db91fcf4fb3043601ca44e
erics118 <52634785+erics118@users.noreply.github.com>
2024-06-09 12:00:37 -0400

    refactor: separate logic out into individual functions

--- src/client/scenes/Game.ts
+++ src/client/scenes/Game.ts
@@ -81,9 +81,8 @@ export class Game extends Phaser.Scene {
     this.room.send(MessageType.HEALTH, msg);
   }
 
-  preload() {
-    this.cameras.main.setBackgroundColor(colors.background);
-
+  // create graphics
+  preloadGraphics() {
     // player circle
     this.make
       .graphics({ x: 0, y: 0 })
@@ -143,37 +142,31 @@ export class Game extends Phaser.Scene {
       .generateTexture("enemyBullet", 30, 30);
   }
 
-  async create() {
-    // set world boundaries
-    this.cameras.main.setBounds(0, 0, MAP_SIZE, MAP_SIZE);
-    this.physics.world.setBounds(0, 0, MAP_SIZE, MAP_SIZE);
+  preload() {
+    this.cameras.main.setBackgroundColor(colors.background);
 
-    // register key input events
-    this.keys = this.input?.keyboard?.addKeys({
-      up: Phaser.Input.Keyboard.KeyCodes.UP,
-      down: Phaser.Input.Keyboard.KeyCodes.DOWN,
-      left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-      right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-      w: Phaser.Input.Keyboard.KeyCodes.W,
-      a: Phaser.Input.Keyboard.KeyCodes.A,
-      s: Phaser.Input.Keyboard.KeyCodes.S,
-      d: Phaser.Input.Keyboard.KeyCodes.D,
-    }) as Keys;
+    this.preloadGraphics();
+  }
 
-    // #region draw graphics: background grid, debug menu, username
-
-    // draw background grid
+  // create background grid
+  createBackground() {
     this.grid = this.add
       .grid(MAP_SIZE / 2, MAP_SIZE / 2, MAP_SIZE, MAP_SIZE, GRID_SIZE, GRID_SIZE, colors.gridLines)
       .setDepth(Depth.Background);
+  }
 
+  // create debug menu
+  createDebugMenu() {
     // draw debug menu
     this.debugMenu = this.add
       .text(4, 4, "", { color: colors.debug.text })
       // don't more relative to player
       .setScrollFactor(0)
       .setDepth(Depth.Overlay);
+  }
 
+  // create player username and health bar
+  createUserOverlay() {
     // draw username
     this.username = this.add
       .text(this.game.scale.width / 2, this.game.scale.height - 70, this.usernameStr, {
@@ -195,10 +188,10 @@ export class Game extends Phaser.Scene {
       // don't move relative to player
       .setScrollFactor(0)
       .setDepth(Depth.Overlay);
+  }
 
-    // #endregion
-
-    // #region minimap
+  // create minimap
+  createMinimap() {
     this.minimap = this.cameras
       .add(
         this.game.scale.width - MINIMAP_SIZE - 20,
@@ -213,145 +206,170 @@ export class Game extends Phaser.Scene {
     // keep only players on minimap
     this.minimap.ignore([this.debugMenu, this.grid]);
 
-    // update minimap position on window resize
-    window.addEventListener(
-      "resize",
-      () => {
-        this.minimap.setPosition(
-          this.game.scale.width - MINIMAP_SIZE - 20,
-          this.game.scale.height - MINIMAP_SIZE - 20,
-        );
-        this.username.setPosition(this.game.scale.width / 2, this.game.scale.height - 70);
-        this.healthBar.setPosition(this.game.scale.width / 2, this.game.scale.height - 40);
-      },
-      false,
-    );
-
     this.minimap.setBackgroundColor(colors.minimap);
     this.minimap.scrollX = MAP_SIZE / 2;
     this.minimap.scrollY = MAP_SIZE / 2;
-    // #endregion
+  }
 
-    // #region connect to server and draw players
+  // update minimap, username, and health bar position on window resize
+  onResize() {
+    this.minimap.setPosition(
+      this.game.scale.width - MINIMAP_SIZE - 20,
+      this.game.scale.height - MINIMAP_SIZE - 20,
+    );
+    this.username.setPosition(this.game.scale.width / 2, this.game.scale.height - 70);
+    this.healthBar.setPosition(this.game.scale.width / 2, this.game.scale.height - 40);
+  }
 
-    // connect with the room
+  // listen for new players and create player entities
+  // includes listeners for player and bullet updates
+  onPlayerAdd(player: Player, sessionId: string) {
+    this.bulletEntities[sessionId] = {};
+
+    // current player
+    if (sessionId === this.room.sessionId) {
+      const entity = this.physics.add
+        .image(player.x, player.y, "playerCircle")
+        .setDepth(Depth.Players);
+
+      this.playerEntities[sessionId] = entity;
+
+      entity.setCollideWorldBounds(true);
+
+      // make camera follow player
+      this.cameras.main.startFollow(entity, true, 0.08, 0.08);
+
+      // create local and remote debug rectangles
+      this.localRef = this.add
+        .rectangle(0, 0, entity.width, entity.height)
+        .setDepth(Depth.Players)
+        .setStrokeStyle(1, colors.debug.local);
+
+      this.remoteRef = this.add
+        .rectangle(0, 0, entity.width, entity.height)
+        .setDepth(Depth.Players)
+        .setStrokeStyle(1, colors.debug.remote);
+
+      player.onChange(() => {
+        this.remoteRef.x = player.x;
+        this.remoteRef.y = player.y;
+      });
+
+      player.bullets.onAdd((bullet: Bullet, bulletId) => {
+        const entity = this.physics.add
+          .image(bullet.x, bullet.y, "playerBullet")
+          .setDepth(Depth.Bullets);
+
+        this.bulletEntities[sessionId][bulletId] = entity;
+      });
+    } else {
+      const entity = this.physics.add
+        .image(player.x, player.y, "enemyCircle")
+        .setDepth(Depth.Players);
+      this.playerEntities[sessionId] = entity;
+
+      // listen for server updates
+      player.onChange(() => {
+        // LERP the positions during the render loop.
+        entity.setData("serverX", player.x);
+        entity.setData("serverY", player.y);
+        entity.setData("serverRotation", player.rotation);
+      });
+
+      player.bullets.onAdd((bullet: Bullet, bulletId: string) => {
+        const entity = this.physics.add
+          .image(bullet.x, bullet.y, "enemyBullet")
+          .setDepth(Depth.Bullets);
+
+        this.bulletEntities[sessionId][bulletId] = entity;
+
+        bullet.onChange(() => {
+          entity.setData("serverX", bullet.x);
+          entity.setData("serverY", bullet.y);
+        });
+      });
+    }
+  }
+
+  // remove local reference when entity is removed from the server
+  onPlayerRemove(_player: Player, sessionId: string) {
+    const entity = this.playerEntities[sessionId];
+    if (entity) {
+      entity.destroy();
+      delete this.playerEntities[sessionId];
+    }
+  }
+
+  // create bullet when space is pressed
+  onKeydownSpace() {
+    if (this.lastBulletTick + this.reloadTicks < this.currentTick) {
+      const msg: BulletMessage = {
+        rotation: this.playerEntities[this.room.sessionId].rotation,
+        x: this.playerEntities[this.room.sessionId].x,
+        y: this.playerEntities[this.room.sessionId].y,
+      };
+
+      this.sendBulletMessage(msg);
+
+      this.lastBulletTick = this.currentTick;
+    }
+  }
+
+  // rotate player entity and send message to server
+  onPointerMove(pointer: Phaser.Input.Pointer) {
+    this.pointerLocation = { x: pointer.worldX, y: pointer.worldY };
+
+    const pointerAngle = Phaser.Math.Angle.Between(
+      this.playerEntities[this.room.sessionId].x,
+      this.playerEntities[this.room.sessionId].y,
+      pointer.worldX,
+      pointer.worldY,
+    );
+    this.playerEntities[this.room.sessionId].setRotation(pointerAngle);
+
+    this.sendRotateMessage({
+      rotation: pointerAngle,
+    });
+  }
+
+  async create() {
+    // set world boundaries
+    this.cameras.main.setBounds(0, 0, MAP_SIZE, MAP_SIZE);
+    this.physics.world.setBounds(0, 0, MAP_SIZE, MAP_SIZE);
+
+    // register key input events
+    this.keys = this.input?.keyboard?.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.UP,
+      down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+      left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+      w: Phaser.Input.Keyboard.KeyCodes.W,
+      a: Phaser.Input.Keyboard.KeyCodes.A,
+      s: Phaser.Input.Keyboard.KeyCodes.S,
+      d: Phaser.Input.Keyboard.KeyCodes.D,
+    }) as Keys;
+
+    this.createBackground();
+
+    this.createDebugMenu();
+
+    this.createUserOverlay();
+
+    this.createMinimap();
+
+    // update minimap position on window resize
+    window.addEventListener("resize", this.onResize.bind(this), false);
+
     await this.connect();
 
-    // listen for new players
-    this.room.state.players.onAdd((player: Player, sessionId: string) => {
-      this.bulletEntities[sessionId] = {};
+    this.room.state.players.onAdd(this.onPlayerAdd.bind(this));
 
-      // current player
-      if (sessionId === this.room.sessionId) {
-        const entity = this.physics.add
-          .image(player.x, player.y, "playerCircle")
-          .setDepth(Depth.Players);
-        this.playerEntities[sessionId] = entity;
+    this.room.state.players.onRemove(this.onPlayerRemove.bind(this));
 
-        // this.playerEntities[this.room.sessionId] = entity;
-
-        entity.setCollideWorldBounds(true);
-
-        // make camera follow it
-        this.cameras.main.startFollow(entity, true, 0.08, 0.08);
-
-        // create local and remote debug rectangles
-        this.localRef = this.add
-          .rectangle(0, 0, entity.width, entity.height)
-          .setDepth(Depth.Players);
-        this.localRef.setStrokeStyle(1, colors.debug.local);
-
-        this.remoteRef = this.add
-          .rectangle(0, 0, entity.width, entity.height)
-          .setDepth(Depth.Players);
-        this.remoteRef.setStrokeStyle(1, colors.debug.remote);
-
-        player.onChange(() => {
-          this.remoteRef.x = player.x;
-          this.remoteRef.y = player.y;
-        });
-        player.bullets.onAdd((bullet: Bullet, bulletId) => {
-          const entity = this.physics.add
-            .image(bullet.x, bullet.y, "playerBullet")
-            .setDepth(Depth.Bullets);
-
-          this.bulletEntities[sessionId][bulletId] = entity;
-        });
-      } else {
-        const entity = this.physics.add
-          .image(player.x, player.y, "enemyCircle")
-          .setDepth(Depth.Players);
-        this.playerEntities[sessionId] = entity;
-
-        // listen for server updates
-        player.onChange(() => {
-          // LERP the positions during the render loop.
-          entity.setData("serverX", player.x);
-          entity.setData("serverY", player.y);
-          entity.setData("serverRotation", player.rotation);
-        });
-
-        player.bullets.onAdd((bullet: Bullet, bulletId: string) => {
-          const entity = this.physics.add
-            .image(bullet.x, bullet.y, "enemyBullet")
-            .setDepth(Depth.Bullets);
-
-          this.bulletEntities[sessionId][bulletId] = entity;
-
-          bullet.onChange(() => {
-            entity.setData("serverX", bullet.x);
-            entity.setData("serverY", bullet.y);
-          });
-        });
-      }
-    });
-
-    // remove local reference when entity is removed from the server
-    this.room.state.players.onRemove((_player: Player, sessionId: string) => {
-      const entity = this.playerEntities[sessionId];
-      if (entity) {
-        entity.destroy();
-        delete this.playerEntities[sessionId];
-      }
-    });
-    // #endregion
-
-    // #region draw new player bullet if space is pressed
-    this.input.keyboard.on(
-      "keydown-SPACE",
-      () => {
-        if (this.lastBulletTick + this.reloadTicks < this.currentTick) {
-          const msg: BulletMessage = {
-            rotation: this.playerEntities[this.room.sessionId].rotation,
-            x: this.playerEntities[this.room.sessionId].x,
-            y: this.playerEntities[this.room.sessionId].y,
-          };
-
-          this.sendBulletMessage(msg);
-
-          this.lastBulletTick = this.currentTick;
-        }
-      },
-      this,
-    );
-    // #endregion
+    this.input.keyboard.on("keydown-SPACE", this.onKeydownSpace.bind(this), this);
 
     // register pointer move event
-    this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
-      this.pointerLocation = { x: pointer.worldX, y: pointer.worldY };
-
-      const pointerAngle = Phaser.Math.Angle.Between(
-        this.playerEntities[this.room.sessionId].x,
-        this.playerEntities[this.room.sessionId].y,
-        pointer.worldX,
-        pointer.worldY,
-      );
-      this.playerEntities[this.room.sessionId].setRotation(pointerAngle);
-
-      this.sendRotateMessage({
-        rotation: pointerAngle,
-      });
-    });
+    this.input.on("pointermove", this.onPointerMove.bind(this));
   }
 
   async connect() {
@@ -399,18 +417,9 @@ export class Game extends Phaser.Scene {
     this.healthBar.text = `Health: ${this.room.state.players.get(this.room.sessionId).health}`;
   }
 
-  fixedTick(_time: number, _delta: number) {
-    this.currentTick++;
+  // move local player and bullets
+  moveLocal(msg: MoveMessage) {
     const velocity = 2;
-
-    const msg: MoveMessage = {
-      up: this.keys.up.isDown || this.keys.w.isDown,
-      down: this.keys.down.isDown || this.keys.s.isDown,
-      left: this.keys.left.isDown || this.keys.a.isDown,
-      right: this.keys.right.isDown || this.keys.d.isDown,
-    };
-    this.sendMoveMessage(msg);
-
     // #region move current player
     if (msg.left) {
       this.playerEntities[this.room.sessionId].x -= velocity;
@@ -423,8 +432,6 @@ export class Game extends Phaser.Scene {
     } else if (msg.down) {
       this.playerEntities[this.room.sessionId].y += velocity;
     }
-
-    // #endregion
 
     for (const [bulletId, entity] of Object.entries(this.bulletEntities[this.room.sessionId])) {
       const bullet = this.room.state.players.get(this.room.sessionId).bullets.get(bulletId);
@@ -439,13 +446,9 @@ export class Game extends Phaser.Scene {
       entity.x += Math.cos(bullet.rotation) * 5;
       entity.y += Math.sin(bullet.rotation) * 5;
     }
-    // #region update remote player locations
+  }
 
-    // draw local debug ref
-    // console.log(this.room.sessionId)
-    this.localRef.x = this.playerEntities[this.room.sessionId].x;
-    this.localRef.y = this.playerEntities[this.room.sessionId].y;
-
+  moveRemote() {
     for (const [sessionId, _] of this.room.state.players) {
       // interpolate all player entities except fof the current player
       if (sessionId === this.room.sessionId) {
@@ -475,6 +478,26 @@ export class Game extends Phaser.Scene {
         entity.y = Phaser.Math.Linear(entity.y, serverY, 0.2);
       }
     }
-    // #endregion
+  }
+
+  fixedTick(_time: number, _delta: number) {
+    this.currentTick++;
+
+    const msg: MoveMessage = {
+      up: this.keys.up.isDown || this.keys.w.isDown,
+      down: this.keys.down.isDown || this.keys.s.isDown,
+      left: this.keys.left.isDown || this.keys.a.isDown,
+      right: this.keys.right.isDown || this.keys.d.isDown,
+    };
+
+    this.sendMoveMessage(msg);
+
+    this.moveLocal(msg);
+
+    // draw local debug ref
+    this.localRef.x = this.playerEntities[this.room.sessionId].x;
+    this.localRef.y = this.playerEntities[this.room.sessionId].y;
+
+    this.moveRemote();
   }
 }
--- src/client/scenes/StartScreen.ts
+++ src/client/scenes/StartScreen.ts
@@ -2,12 +2,33 @@ import Phaser from "phaser";
 import type { SceneData } from "#shared/types";
 
 export class StartScreen extends Phaser.Scene {
+  textEntry: Phaser.GameObjects.Text;
+  
   constructor() {
     super({ key: "selector", active: true });
   }
 
   preload() {
     this.cameras.main.setBackgroundColor(0x000000);
+  }
+
+  onKeydown(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      this.startGame();
+    } else if (event.key === "Backspace") {
+      this.textEntry.text = this.textEntry.text.substring(0, this.textEntry.text.length - 1);
+    } else if (
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=[]\\;',./~!@#$%^&*()_+{}|:\"<>? ".includes(
+        event.key,
+      )
+    ) {
+      this.textEntry.text += event.key;
+    }
+  }
+
+  startGame() {
+    this.scene.stop("selector");
+    this.scene.start("diep", { username: this.textEntry.text } as SceneData);
   }
 
   create() {
@@ -21,25 +42,12 @@ export class StartScreen extends Phaser.Scene {
       .text(30, 100, "Start Game", textStyle)
       .setInteractive()
       .setPadding(6)
-      .on("pointerdown", () => {
-        this.scene.stop("selector");
-        this.scene.start("diep", { username: textEntry.text } as SceneData);
-      });
+      .on("pointerdown", this.startGame.bind(this));
 
     this.add.text(30, 300, "Enter your name:", textStyle);
 
-    const textEntry = this.add.text(30, 500, "", textStyle);
+    this.textEntry = this.add.text(30, 500, "", textStyle);
 
-    this.input.keyboard.on("keydown", (event: KeyboardEvent) => {
-      if (event.key === "Backspace") {
-        textEntry.text = textEntry.text.substring(0, textEntry.text.length - 1);
-      } else if (
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=[]\\;',./~!@#$%^&*()_+{}|:\"<>? ".includes(
-          event.key,
-        )
-      ) {
-        textEntry.text += event.key;
-      }
-    });
+    this.input.keyboard.on("keydown", this.onKeydown.bind(this));
   }
 }
